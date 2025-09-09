0e048f7808376031b8051ce36afb4adec14fcfcd
Ashu saini <ashusaini514@gmail.com>
2025-09-09 15:02:00 +0530

    MERN_STACK:Added nodemon,webpack,babel configurations, basic server with node , express , mongodb and basic react template for root url

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import devBundle from './devBundle'
+import path from 'path'
+import template from './../template'
+import {MongoClient} from 'mongodb'
+
+
+const CURRENT_WORKING_DIR = process.cwd()
+let port = process.env.PORT || 3000
+
+const url =  process.env.MONGODB_URI || 'mongodb://localhost:27017/mern_stack'
+
+MongoClient.connect(url,(err,db)=>{
+    console.log("Connected successfully to mongodb server")
+    db.close()
+})
+
+const app = express()
+app.use('/dist',express.static(path.join(CURRENT_WORKING_DIR,'dist')))
+
+app.get('/',(req,res) => {
+    res.status(200).send(template())
+})
+devBundle.compile(app)
+
+app.listen(port,function onStart(err){
+    if(err){
+        console.log(err)
+    }
+    console.info('Server started on port %s',port)
+})
